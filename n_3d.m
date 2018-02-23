ad807972533654e66d3032a4147e418c5c6e78a5
milan <send2milankumar@gmail.com>
2018-02-24 00:59:33 +0530

    Three dimensional Cyclic Pursuit
    if N agents are left randomly anywhere in space and they are made to follow each other at a pi/N angle with the line joining immediate next agent, then they converge to a circular trajectory

---
+++ n_3d.m
@@ -0,0 +1,30 @@
+% N agents randomly left in 3D space
+% If they follow each other at pi/n angle, they finally converge to a circular trajectory
+
+%each page contains coordinates of each agent at different times
+n=input('number of agents');             % take n as input
+al=pi/n;                                 %initial offset
+k=input('controller gain');              %controller gain
+T=[cos(al),sin(al) 0;-sin(al),cos(al) 0;0 0 1];    %trasformation matrix
+x(1:n)=input('initial x coordinates');
+y(1:n)=input('initial y coordinates');
+zz(1:n)=input('initial z coordinates');
+for i=1:n 
+    z(1:3,i)=[x(i);y(i);zz(i)];
+end
+store_position=ones(3,100000,n);            %for preallocation purpose
+for t=1:1:100000
+    for i=1:n
+        u(1:3,i)=(k*T*(z(1:3,mod(i,n)+1)-z(1:3,i)));
+    end
+    for i=1:n
+        z(1:3,i)=u(1:3,i).*0.001 + z(1:3,i);
+        store_position(1:3,t,i)=z(1:3,i);
+    end
+end
+for i=1:n
+    plot3(store_position(1,:,i),store_position(2,:,i),store_position(3,:,i));
+    hold on;
+end
+        
+    
